6a601d7390fba255b9b25b1ef929945bb704c82b
Adam Johnson <adamwjo@gmail.com>
2018-06-21 14:30:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0,)
-  return array
+  return array.slice(1)
 }
