688482bd453f99a230320c9a83819934063c3e19
Adam Johnson <adamwjo@gmail.com>
2018-06-21 12:37:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+]
+
+function AddElementToBeginningOfArray(array, element) {
+  element.unshift
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArrayaddElementToBeginningOfArray(array, element) {
+  element.unshift
+  return array
+}
