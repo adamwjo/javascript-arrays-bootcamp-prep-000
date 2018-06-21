2ba72766c2b2aac10378d41411ad7ebaf9139bfd
Adam Johnson <adamwjo@gmail.com>
2018-06-21 13:11:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+  return array
+}
