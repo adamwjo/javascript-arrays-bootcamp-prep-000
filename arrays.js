0b83c18a9f8dff1186b79e4a3ddb9a5bf63f8cbd
Adam Johnson <adamwjo@gmail.com>
2018-06-21 14:07:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,13 @@ function addElementToBeginningOfArray(array, element) {
   array = [element, ...array]
   return array
 }
+
+unction destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function AddElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
