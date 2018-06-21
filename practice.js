e01b41b33f9ba8200b8ad1f16867ad41400cdbfd
Adam Johnson <adamwjo@gmail.com>
2018-06-21 14:06:01 +0000

    automatically backed up by learn

---
+++ practice.js
@@ -0,0 +1,48 @@
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+]
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function AddElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+}
+
+function removeElementFromEndOfArray(array) {
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+
+}
